ac58c7b82761892034e2deaab3ee821d4582c0e7
ugesh <ugesh.roopchand@hotmail.com>
2022-04-14 19:24:05 +0400

    Starting

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "./Header";
+
+
+class TodoContainer extends React.Component {
+ render(){
+     return(
+
+        <div className="globalcontainer">
+
+            <div className="container">
+                <Header/>
+            </div>
+
+        </div>
+     )
+ }
+}
+
+
+export default TodoContainer
